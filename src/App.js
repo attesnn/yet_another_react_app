5bb92090fed9c1a5dec53896963665f5b8eccecb
attesnn <attesnn@gmail.com>
2023-01-24 21:27:24 +0200

    with tailwind

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
 import './App.css';
 import { Auth } from './components/Auth';
+import Cookies from 'universal-cookie';
+import styles from "./index.css"
+const cookies = new Cookies()
 
 function App() {
   const [isAuth, setIsAuth] = useState(cookies.get("auth-token"))
-  const [room, setRoom] = useState ("")
+  const [room, setRoom] = useState (null)
 
   if (!isAuth){
     return (
@@ -12,26 +15,22 @@ function App() {
         <Auth/>
       </div>
     );
-
-
   }
 
   return (
-  <div>
+  <div className="container mx-auto bg-gray-200 rounded-xl">
     {room ? (<div> Chat </div>
     ) : (
-    <div>
+    <div className="m-5 p-10">
       <label> Enter Room Name :  </label>
       <input/>
       <button></button>
-    </div>
-    ) 
-     <div>  }
+    </div>    
+    )     
+    }
+    <p className="text-3xl text-gray-700 font-bold mb-5 py-9">Content</p>
   </div>
-  )
-  
+  );     
 }
-
-
 
 export default App;
